e14afedbf020707f7361ab72255e40387bc09635
Caio Alfonso <caioap25@gmail.com>
2022-04-21 23:04:36 -0300

    Added advantages section to studios page

---
+++ components/studios/AdvantagesSection.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="advantages" aria-labelledby="title">
+    <header>
+      <h2 id="title" class="title">
+        <span>Vantagens</span> <br />
+        de ser <span class="musicalll">Musicalll</span>
+      </h2>
+    </header>
+
+    <div class="body">
+      <nuxt-img src="/images/studios-advantages.png" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.advantages {
+  margin-top: 6rem;
+
+  header {
+    .title {
+      font-size: 2.5rem;
+      text-align: center;
+      color: $text-light-clr-1;
+
+      span:first-of-type {
+        font-family: $font-rampage-monoline;
+        font-size: 8rem;
+        line-height: 1;
+      }
+    }
+  }
+
+  .body {
+    margin-top: 4rem;
+
+    img {
+      margin: auto;
+    }
+  }
+}
+</style>
--- pages/studios.vue
+++ pages/studios.vue
@@ -3,14 +3,21 @@
     <BaseHero image="/images/hero-background-studios.png"/>
 
     <StudiosSection />
+
+    <AdvantagesSection />
   </div>
 </template>
 
 <script>
 import StudiosSection from '~/components/studios/StudiosSection.vue'
+import AdvantagesSection from '~/components/studios/AdvantagesSection.vue'
+
 export default {
   name: 'StudiosPage',
-  components: { StudiosSection }
+  components: { 
+    StudiosSection,
+    AdvantagesSection
+  }
 }
 </script>
 
